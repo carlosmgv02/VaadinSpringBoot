c30509c2a8ca5fdbfc734c8f23fd4e2927991ab6
Carlos Martínez <carlos.martinezg@estudiants.urv.cat>
2022-11-17 13:38:42 +0100

    files added

--- src/main/java/com/example/application/views/list/ListView.java
+++ src/main/java/com/example/application/views/list/ListView.java
@@ -1,5 +1,6 @@
 package com.example.application.views.list;
 
+import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.html.Paragraph;
@@ -12,19 +13,6 @@ import com.vaadin.flow.router.Route;
 public class ListView extends VerticalLayout {
 
     public ListView() {
-        setSpacing(false);
-
-        Image img = new Image("images/empty-plant.png", "placeholder plant");
-        img.setWidth("200px");
-        add(img);
-
-        add(new H2("This place intentionally left empty"));
-        add(new Paragraph("It’s a place where you can grow your own UI 🤗"));
-
-        setSizeFull();
-        setJustifyContentMode(JustifyContentMode.CENTER);
-        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
-        getStyle().set("text-align", "center");
+        add(new H1("Hello world"));
     }
-
 }
